5588f649a31dc72f39c337f3e04de57fb08117d0
KunAce <wxkace@gmail.com>
2018-11-29 13:06:58 -0800

    clean columns 'forked_from' and 'deleted'

--- src/data_clean.py
+++ src/data_clean.py
@@ -15,7 +15,7 @@ def clean(df_chunk):
     # print(df_chunk['description'].isnull().value_counts())
     df_chunk['description'].replace({'\\N': np.nan}, inplace = True)
     df_chunk.dropna(subset=['description'], inplace = True) # NaN data
-    # print(df_chunk['description'].isnull().value_counts())
+
 
     # 1) clean 'forked from'
     # Only not 'forked_from' records are kept (with '\N')
@@ -25,18 +25,18 @@ def clean(df_chunk):
     df_chunk = df_chunk[df_chunk['deleted'] == 0]
 
     # 3) clean 'unknown' column
-    df_chunk = df_chunk.drop('unknown', 1)
+    df_chunk.drop('unknown', axis = 1, inplace = True)
 
-    # 4) re-format the 'url' column to make the links usable
+    # 4) clean 'forked_from' and 'deleted'
+    df_chunk.drop('forked_from', axis = 1, inplace = True)
+    df_chunk.drop('deleted', axis = 1, inplace = True)
+
+    # 5) re-format the 'url' column to make the links usable
     df_chunk['url'].replace({'api.': ''}, inplace=True, regex=True)
     df_chunk['url'].replace({'repos/': ''}, inplace=True, regex=True)
 
-    # 5) deleted 'forked_from' and 'deleted' columns because they won't be needed for later use
-    
-
-
     # Output the new CSV file
-    df_chunk.to_csv('../dataset/project_test_new.csv', sep=',',mode='a')
+    df_chunk.to_csv('../dataset/project_clean.csv', sep=',',mode='a')
 
 
 
@@ -48,16 +48,16 @@ col_names=['id','url','owner_id','name','description','language','created_at','f
 reader = pd.read_csv('../dataset/projects.csv', skiprows = 1, iterator = True, error_bad_lines = False, names = col_names)
 
 if_loop = True
-chunk_size = 1000
-number_of_chunk = 0
+chunk_size = 200000
+# number_of_chunk = 0
 while if_loop:
     try:
         df_chunk = pd.DataFrame(reader.get_chunk(chunk_size))
         clean(df_chunk)
-        number_of_chunk += 1
-        if (number_of_chunk == 1 ):
-            if_loop = False
-            break
+        # number_of_chunk += 1
+        # if (number_of_chunk == 1 ):
+        #     if_loop = False
+        #     break
     except StopIteration:
         if_loop = False
         print("Iteration is stopped.")
