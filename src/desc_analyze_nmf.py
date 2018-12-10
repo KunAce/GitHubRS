f83de8a44c53ead350ae997e28c86e07f120a9ae
KunAce <wxkace@gmail.com>
2018-12-10 13:34:53 -0800

    the function to generate NMF models (version 1 and version 2) (1 with the technique of lemmatization from LDA , version 2 with its own technique of stemming)

---
+++ src/desc_analyze_nmf.py
@@ -0,0 +1,131 @@
+import pandas as pd
+import numpy as np
+
+
+from nltk.corpus import stopwords
+import nltk
+
+from gensim.models import ldamodel
+import gensim.corpora
+
+import pickle
+
+#Gensim
+import gensim
+import gensim.corpora as corpora
+from gensim.utils import simple_preprocess
+from gensim.models import CoherenceModel
+
+# Spacy for lemmatization
+import spacy
+
+# Prepare Stopwords
+
+from nltk.corpus import stopwords
+stop_words = stopwords.words('english')
+stop_words.extend(['from','re','edu','use'])
+
+
+# Import the 'description' data
+
+col_names = ['name','description']
+reader = pd.read_csv('../dataset/desc_en.csv', encoding='utf_8_sig', names=col_names,skiprows = 1, nrows = 1000)
+
+# Convert each row of description to list
+desc_list = reader['description'].values.tolist()
+
+
+# Tokenize words and Clean-up Text
+
+def desc_to_words(desc_list):
+    for desc_item in desc_list:
+        yield(gensim.utils.simple_preprocess(str(desc_item),deacc=True)) # remove punctuations
+
+desc_words=list(desc_to_words(desc_list))
+
+
+# Creating Bigram and Tirgram Models
+
+bigram = gensim.models.Phrases(desc_words, min_count=5, threshold=100) # higher threshold fewer phrases
+trigram = gensim.models.Phrases(bigram[desc_words], threshold=100)
+
+# Faster way to get a sentence clubbed as a trigram/bigram
+bigram_mod= gensim.models.phrases.Phraser(bigram)
+trigram_mod=gensim.models.phrases.Phraser(trigram)
+
+
+# Remove stopwords and Lemmatize
+
+# Define functions for stopwords,bigrams, trigrams and lemmatization
+def remove_stopwords(texts):
+    return [[word for word in simple_preprocess(str(doc)) if word not in stop_words] for doc in texts]
+
+def make_bigrams(texts):
+    return [bigram_mod[doc] for doc in texts]
+
+def make_trigrams(texts):
+    return [trigram_mod[bigram_mod[doc]] for doc in texts]
+
+def lemmatization(texts, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV']):
+
+    texts_out = []
+    for sent in texts:
+        doc = nlp(" ".join(sent))
+        texts_out.append([token.lemma_ for token in doc if token.pos_ in allowed_postags])
+    return texts_out
+
+
+# Remove Stop Words
+desc_words_nostops = remove_stopwords(desc_words)
+
+# Form Bigrams
+desc_words_bigrams = make_bigrams(desc_words_nostops)
+
+nlp = spacy.load('en', disable=['parser', 'ner'])
+
+# Do lemmatization keeping only noun, adj, vb, adv
+desc_lemmatized = lemmatization(desc_words_bigrams, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV'])
+
+
+# Vectorization => TF-IDF
+# from sklearn.feature_extraction.text import CountVectorizer
+# vectorizer = CountVectorizer()
+# A = vectorizer.fit_transform(desc_lemmatized)
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer()
+
+desc_lemmatized_str = [' '.join(text) for text in desc_lemmatized]
+A = vectorizer.fit_transform(desc_lemmatized_str)
+
+
+# Access the list of all terms and an associated dictionary (vocabulary_)
+# which maps each unique term to a corresponding column in the matrix.
+terms = vectorizer.get_feature_names()
+# len(terms)
+
+vocab = vectorizer.vocabulary_
+# vocab["world"]
+
+# Number of Topics
+k = 50
+
+from sklearn import decomposition
+model = decomposition.NMF(n_components=k, init="nndsvd")
+W = model.fit_transform(A)
+H = model.components_
+
+
+
+
+# import numpy as np
+top_indices = np.argsort( H[topic_index,:] )[::-1]
+top_terms = []
+for term_index in top_indices[0:top]:
+    top_terms.append( terms[term_index] )
+
+
+# top_indices = np.argsort( W[:,topic_index] )[::-1]
+top_documents = []
+for doc_index in top_indices[0:top]:
+    top_documents.append( desc_lemmatized_str[doc_index] )
+
---
+++ src/desc_analyze_nmf_v2.py
@@ -0,0 +1,68 @@
+from __future__ import print_function
+from time import time
+
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.decomposition import NMF
+import pandas as pd
+
+
+n_samples = 1000
+n_features = 1000
+n_topics = 50
+n_top_words = 10
+
+
+def print_top_words(model, feature_names, n_top_words):
+    for topic_idx, topic in enumerate(model.components_):
+        message = "Topic #%d: " % topic_idx
+        message += " ".join([feature_names[i]
+                             for i in topic.argsort()[:-n_top_words - 1:-1]])
+        print(message)
+    print()
+
+print("Loading dataset...")
+
+# Import the 'description' data
+
+col_names = ['name','description']
+reader = pd.read_csv('../dataset/desc_en.csv', encoding='utf_8_sig', names=col_names,skiprows = 1, nrows = 1000)
+
+# Remove non-english by regular expressions
+# Convert each row of description to list
+desc_list = reader['description'].values.tolist()
+
+# Use tf-idf features for NMF.
+print("Extracting tf-idf features for NMF...")
+tfidf_vectorizer = TfidfVectorizer(max_df=0.95, min_df=2,
+                                   max_features=n_features,
+                                   stop_words='english')
+t0 = time()
+tfidf = tfidf_vectorizer.fit_transform(desc_list)
+print("done in %0.3fs." % (time() - t0))
+
+# Fit the NMF model
+print("Fitting the NMF model (Frobenius norm) with tf-idf features, "
+      "n_samples=%d and n_features=%d..."
+      % (n_samples, n_features))
+t0 = time()
+nmf = NMF(n_components=n_topics, random_state=1,
+          alpha=.1, l1_ratio=.5).fit(tfidf)
+print("done in %0.3fs." % (time() - t0))
+
+print("\nTopics in NMF model (Frobenius norm):")
+tfidf_feature_names = tfidf_vectorizer.get_feature_names()
+print_top_words(nmf, tfidf_feature_names, n_top_words)
+
+# Fit the NMF model
+print("Fitting the NMF model (generalized Kullback-Leibler divergence) with "
+      "tf-idf features, n_samples=%d and n_features=%d..."
+      % (n_samples, n_features))
+t0 = time()
+nmf = NMF(n_components=n_topics, random_state=1,
+          beta_loss='kullback-leibler', solver='mu', max_iter=1000, alpha=.1,
+          l1_ratio=.5).fit(tfidf)
+print("done in %0.3fs." % (time() - t0))
+
+print("\nTopics in NMF model (generalized Kullback-Leibler divergence):")
+tfidf_feature_names = tfidf_vectorizer.get_feature_names()
+print_top_words(nmf, tfidf_feature_names, n_top_words)
